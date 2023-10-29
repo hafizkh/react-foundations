be0db73b38412a82677992eacf57a6603e01ff68
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-10-30 00:00:25 +0200

    Started React Series developed by Akshay

---
+++ App.js
@@ -0,0 +1,29 @@
+// const h1Tag = React.createElement("h1", { id: "heading" }, "Hello World from React")
+
+// console.log(h1Tag)
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(h1Tag)
+
+const parent = React.createElement("div", { id: "parent" },
+    [
+        React.createElement("div", { id: "child" },
+            [
+                React.createElement("h1", {}, "I am an h1 tag"),
+                React.createElement("h2", {}, "I am an h2 tag")
+            ]),
+        React.createElement("div", { id: "child2" },
+            [
+                React.createElement("h1", {}, "I am an h1 tag"),
+                React.createElement("h2", {}, "I am an h2 tag")
+            ])
+    ]
+
+)
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
