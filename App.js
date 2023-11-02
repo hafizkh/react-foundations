968b1a0cef49e0fe8f44ea0e7746c2724d875f80
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-11-02 23:27:18 +0200

    Talk is cheap, show me code

--- App.js
+++ App.js
@@ -1,45 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const name = "Hafiz"
-const age = 37
-const greet = <p>Hello {name} and you are {age} years old</p>
+/*
+Header
+
+-logo
+- nav items
+
+Body
+- search
+- Restaurant container
+    - Restaurant card
+        - image
+        - name,cuisines
+        - rating
+
+Footer
+- copy right
+- links
+- address
+- contact
+*/
+const styleCard = {
+    width: "200px",
+    height: "300px",
+    backgroundColor: "#f0f0f0",
+    margin: "5px",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px 0 rgb(163, 145, 145)",
+    overflow: "hidden",
+    padding: "9px"
+
+}
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-            <NavBar />
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg" alt="Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-const Footer = () => {
+const RestaurantCard = ({ resName, cuisine }) => {
     return (
-        <div>
-            <h1>Footer</h1>
-            {Header()}
+        <div className="res-card" style={styleCard}>
+            <img className="res-pic" src="https://i.ytimg.com/vi/Ro8s63DKlO0/maxresdefault.jpg" alt="Biryani" />
+            <h2>{resName}</h2>
+            <h3>{cuisine}</h3>
+            <h3>5 star</h3>
+            <h4>30 mins</h4>
         </div>
     )
 }
 
-const NavBar = () => {
+const Body = () => {
     return (
-        <div>
-            <h2>NavBar is inside in Header component</h2>
-            {greet}
-            {/* Example */}
-            {45 - 5}
+        <div className="body">
+            <div className="search">
+                <input type="text" placeholder="Search for food" />
+            </div>
+            <div className="res-container">
+                <RestaurantCard resName="Javed Nehari" cuisine="Naan and Nehari" />
+                <RestaurantCard resName="Mota Chanay Wala" cuisine="Specialist in chanay anda" />
+                <RestaurantCard resName="Jilla Chanay Wala" cuisine="Butter Naan Chanay" />
+            </div>
         </div>
     )
 }
-
 
 const App = () => {
     return (
-        <div>
+        <div className="app">
             <Header />
-            <Footer />
+            <Body />
         </div>
     )
 }
