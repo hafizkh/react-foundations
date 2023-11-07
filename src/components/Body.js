d42829f15eaa1e5147e323c9871765a13f2e6657
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2023-11-07 11:28:06 +0200

    used the api from swiggy to render in the UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,65 +1,21 @@
 import { useState } from "react"
 import RestaurantCard from "./RestaurantCard"
-// import resData from "../utils/apiData"
+import resData from "../utils/apiData"
 
 
 const Body = () => {
-
-    const [resData, setResData] = useState([
-        {
-            "info": {
-                "id": "10208",
-                "name": "Subway",
-                "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-                "costForTwo": "₹350 for two",
-                "cuisines": [
-                    "Burgers",
-                    "American"
-                ],
-                "avgRating": 4.2,
-                "deliveryTime": 18,
-            },
-        },
-        {
-            "info": {
-                "id": "10209",
-                "name": "Burger King",
-                "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-                "costForTwo": "₹350 for two",
-                "cuisines": [
-                    "Burgers",
-                    "American"
-                ],
-                "avgRating": 3.2,
-                "deliveryTime": 18,
-            },
-        },
-        {
-            "info": {
-                "id": "10210",
-                "name": "Nordic Kitchen",
-                "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-                "costForTwo": "₹350 for two",
-                "cuisines": [
-                    "Burgers",
-                    "American"
-                ],
-                "avgRating": 4.2,
-                "deliveryTime": 18,
-            },
-        },])
-
+    const [resList, setResList] = useState(resData)
     return (
         <div>
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
-                    const filteredList = resData.filter((res) => res.info.avgRating > 4)
-                    setResData(filteredList)
+                    const filteredList = resList.filter((res) => res.info.areaName === "Rohini")
+                    setResList(filteredList)
                 }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
                 {
-                    resData.map((resData) => {
+                    resList.map((resData) => {
                         return (
                             <RestaurantCard key={resData.info.id} resData={resData} />
                         )
