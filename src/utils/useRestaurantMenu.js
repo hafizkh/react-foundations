dd82d2e45b099f5b4c4f01a2e8e8ab8e5d557651
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2024-06-20 00:58:49 +0300

    cuxtom hook

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -5,14 +5,13 @@ const useRestaurantMenu = (resId) => {
     const [resTList, setRestList] = useState(null)
     useEffect(() => {
         fetchData()
-    })
+    }, [])
     const fetchData = async () => {
         const data = await fetch(RES_URL_MENU + resId)
         const json = await data.json()
         setRestList(json.data)
     }
     return resTList
-
 }
 
 export default useRestaurantMenu
